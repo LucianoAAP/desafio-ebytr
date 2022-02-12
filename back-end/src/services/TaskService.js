d34b1a18b12cb8c2955b41f6ce72b2a8d98d2b64
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-02-12 19:00:56 -0300

    BACK: adiciona service

---
+++ back-end/src/services/TaskService.js
@@ -0,0 +1,26 @@
+const TasksModel = require('../models/TasksModel');
+const { notFound } = require('../error/apiError');
+
+const findAll = async () => TasksModel.findAll();
+
+const findById = async () => {
+  const task = await TasksModel.findById(id);
+  if (!task) return notFound('Task does not exist');
+  return task;
+};
+
+const create = async (entries) => TasksModel.create(entries);
+
+const update = async (id, entries) => {
+  const task = await TasksModel.findById(id);
+  if (!task) return notFound('Task does not exist');
+  return TasksModel.update(id, entries);
+};
+
+const remove = async (id) => {
+  const task = await TasksModel.findById(id);
+  if (!task) return notFound('Task does not exist');
+  return TasksModel.remove(id);
+};
+
+module.exports = { findAll, findById, create, update, remove };
